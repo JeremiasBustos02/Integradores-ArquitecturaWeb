7aba377d5dcd618bdde11affab1cb2b3a794c4e1
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2025-11-08 15:04:09 -0300

    service monopatin

--- TP_ESPECIAL/microservice-monopatin/src/main/java/com/microservices/microservicemonopatin/service/MonopatinService.java
+++ TP_ESPECIAL/microservice-monopatin/src/main/java/com/microservices/microservicemonopatin/service/MonopatinService.java
@@ -1,4 +1,34 @@
 package com.microservices.microservicemonopatin.service;
 
-public class MonopatinService {
+import com.microservices.microservicemonopatin.dto.MonopatinCercanoDTO;
+import com.microservices.microservicemonopatin.dto.MonopatinDTO;
+import com.microservices.microservicemonopatin.dto.MonopatinRequestDTO;
+import com.microservices.microservicemonopatin.dto.ReporteKilometrosDTO;
+import com.microservices.microservicemonopatin.entity.EstadoMonopatin;
+
+import java.util.List;
+
+public interface MonopatinService {
+
+    MonopatinDTO crear(MonopatinRequestDTO dto);
+    MonopatinDTO obtenerPorId(Long id);
+    List<MonopatinDTO> obtenerTodos();
+    MonopatinDTO actualizar(Long id, MonopatinRequestDTO dto);
+    void eliminar(Long id);
+
+    MonopatinDTO marcarEnMantenimiento(Long id);
+    MonopatinDTO sacarDeMantenimiento(Long id);
+
+    MonopatinDTO cambiarEstado(Long id, EstadoMonopatin nuevoEstado);
+
+    List<MonopatinCercanoDTO> buscarCercanos(Double latitud, Double longitud, Double radioKm);
+
+    List<ReporteKilometrosDTO> reporteKilometros(boolean incluirPausas);
+
+    List<MonopatinDTO> obtenerConMasDeXViajes(int cantidadMinima);
+
+    void actualizarUbicacion(Long id, Double latitud, Double longitud);
+
+    void registrarUsoDeViaje(Long id, Double kmRecorridos, Long tiempoUso, Long tiempoPausas);
+    
 }
