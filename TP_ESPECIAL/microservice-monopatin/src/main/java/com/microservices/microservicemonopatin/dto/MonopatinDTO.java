8ea4da3caee60515957b3cb1f48f415067dc4d66
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2025-11-08 14:41:39 -0300

    DTOS monopatin

--- TP_ESPECIAL/microservice-monopatin/src/main/java/com/microservices/microservicemonopatin/dto/MonopatinDTO.java
+++ TP_ESPECIAL/microservice-monopatin/src/main/java/com/microservices/microservicemonopatin/dto/MonopatinDTO.java
@@ -1,4 +1,19 @@
 package com.microservices.microservicemonopatin.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MonopatinDTO {
+    private Long id;
+    private Double latitud;
+    private Double longitud;
+    private String estado;
+    private Double kmTotales;
+    private Long tiempoUsoTotal;
+    private Boolean enMantenimiento;
+    private Long idParadaActual;
 }
